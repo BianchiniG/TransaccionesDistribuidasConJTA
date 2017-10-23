8f540dc59ca3793978e01dcb965c8c17d0f42307
Emiliano <emilianodemarcoandrada@gmail.com>
2017-10-23 15:52:59 -0300

    prepares antes de commit

--- Punto14.java
+++ Punto14.java
@@ -14,7 +14,7 @@ public class Punto14{
 
     public static void main(String[] argv) {
         try {
-            // Creamos las conexiones 
+            // Creamos las conexiones
             Conexion conexion_alan = new Conexion(
                 ip_alan,"sitioAlan","postgres",
                 "alan","alan"
@@ -31,37 +31,47 @@ public class Punto14{
                 ip_ian,"sitioIan","postgres",
                 "ian","ian"
             );
-            
+
             // Creamos los statements
             Statement stmt_alan = conexion_alan.getConexion().createStatement();
             Statement stmt_emiliano = conexion_emiliano.getConexion().createStatement();
             Statement stmt_german = conexion_german.getConexion().createStatement();
             Statement stmt_ian = conexion_ian.getConexion().createStatement();
-            
-            // 
+
+            //
             conexion_alan.getResource().start(conexion_alan.getXid(), XAResource.TMNOFLAGS);
             conexion_emiliano.getResource().start(conexion_emiliano.getXid(), XAResource.TMNOFLAGS);
             conexion_german.getResource().start(conexion_german.getXid(), XAResource.TMNOFLAGS);
             conexion_ian.getResource().start(conexion_ian.getXid(), XAResource.TMNOFLAGS);
 
-            // 
+            //
             stmt_alan.executeUpdate("insert into cuentas (titular) values ('titular 1')");
             stmt_emiliano.executeUpdate("insert into cuentas (titular) values ('titular 1')");
             stmt_german.executeUpdate("insert into cuentas (titular) values ('titular 1')");
             stmt_ian.executeUpdate("insert into cuentas (titular) values ('titular 1')");
 
-            // 
+            //
             conexion_alan.getResource().end(conexion_alan.getXid(), XAResource.TMSUCCESS);
             conexion_emiliano.getResource().end(conexion_emiliano.getXid(), XAResource.TMSUCCESS);
             conexion_german.getResource().end(conexion_german.getXid(), XAResource.TMSUCCESS);
             conexion_ian.getResource().end(conexion_ian.getXid(), XAResource.TMSUCCESS);
 
-            // 
-            System.out.println(realizaTransaccion(conexion_alan.getResource(), conexion_alan.getXid()));
-            System.out.println(realizaTransaccion(conexion_emiliano.getResource(), conexion_emiliano.getXid()));
-            System.out.println(realizaTransaccion(conexion_german.getResource(), conexion_german.getXid()));
-            System.out.println(realizaTransaccion(conexion_ian.getResource(), conexion_ian.getXid()));
-            
+            //
+            if ( realizaTransaccionPrepare(conexion_alan.getResource(), conexion_alan.getXid()) &&
+            realizaTransaccionPrepare(conexion_emiliano.getResource(), conexion_emiliano.getXid()) &&
+            realizaTransaccionPrepare(conexion_german.getResource(), conexion_german.getXid()) &&
+            realizaTransaccionPrepare(conexion_ian.getResource(), conexion_ian.getXid()) ){
+              conexion_alan.getResource().commit(conexion_alan.getXid(),false);
+              conexion_emiliano.getResource().commit(conexion_emiliano.getXid(),false);
+              conexion_german.getResource().commit(conexion_german.getXid(),false);
+              conexion_ian.getResource().commit(conexion_ian.getXid(),false);
+            }else{
+              conexion_alan.getResource().rollback(conexion_alan.getXid());
+              conexion_emiliano.getResource().rollback(conexion_emiliano.getXid());
+              conexion_german.getResource().rollback(conexion_german.getXid());
+              conexion_ian.getResource().rollback(conexion_ian.getXid());
+            }
+
             //
             stmt_alan.close();
             stmt_emiliano.close();
@@ -84,14 +94,9 @@ public class Punto14{
         }
     }
 
-    public static Boolean realizaTransaccion(XAResource res, Xid xid) throws XAException {
+    public static Boolean realizaTransaccionPrepare(XAResource res, Xid xid) throws XAException {
         int ret = res.prepare(xid);
-
-        if (ret == XAResource.XA_OK) {
-            res.commit(xid, false);
-            return true;
-        }
-        return false;
+        return ret == XAResource.XA_OK;
     }
-}
 
+}
